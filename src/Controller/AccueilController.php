82258aaecb56f3b6ba5916f05099bb72949d3fdf
jordan <jordan-billon@hotmail.fr>
2025-03-03 14:01:51 +0100

    exo 1 symfony

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,7 +9,7 @@ class AccueilController {
     public function negatif(int $nbr1, int $nbr2): Response{
         $total = $nbr1 + $nbr2 ;
         if($total < 0){
-            return new Response('Les nombres sont négatifs');
+            return new Response('Le resultat est négatif');
         }
         return new Response("<p>L’addition de $nbr1 et $nbr2 est égale au résultat : $total </p>");
     }
