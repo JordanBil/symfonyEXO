c1a1992fd6d8c13fdfc1757646036a31ea02d3d9
jordan <jordan-billon@hotmail.fr>
2025-03-03 14:24:08 +0100

    exo 2

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -13,5 +13,27 @@ class AccueilController {
         }
         return new Response("<p>L’addition de $nbr1 et $nbr2 est égale au résultat : $total </p>");
     }
+
+    public function calcul(int $nbr1, int $nbr2, string $ordre){
+        $result = 0;
+        $msg = "";
+        if($ordre == "add"){
+            $result = $nbr1 + $nbr2;
+            $msg = "l'addition";
+        } else if ($ordre == "multi"){
+            $result = $nbr1 * $nbr2;
+            $msg = "la multiplication";
+        } else if ($ordre == "sous"){
+            $result = $nbr1 - $nbr2;
+            $msg = "la soustraction";
+        } else if($ordre == "div"){
+            $result = $nbr1 / $nbr2;
+            $msg = "la division";
+        } else {
+            
+        }
+
+        return new Response("<p>$msg de $nbr1 et $nbr2 est égale a : $result</p>");
+    }
 }
 
